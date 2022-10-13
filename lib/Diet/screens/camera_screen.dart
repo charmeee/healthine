678195dc131ba51837f6f59fb5eaabebdd0d992d
charmeee <apple8841193@naver.com>
2022-10-13 14:12:41 +0900

    add diet

--- lib/Diet/screens/camera_screen.dart
+++ lib/Diet/screens/camera_screen.dart
@@ -135,3 +135,68 @@ class DietResultWidget extends StatelessWidget {
         });
   }
 }
+
+class DietTextForm extends StatefulWidget {
+  const DietTextForm({Key? key}) : super(key: key);
+
+  @override
+  State<DietTextForm> createState() => _DietTextFormState();
+}
+
+class _DietTextFormState extends State<DietTextForm> {
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      width: MediaQuery.of(context).size.width,
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '식단 이름',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '식단 이름을 입력해주세요.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '식단 설명',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '식단 설명을 입력해주세요.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('식단이 등록되었습니다.')),
+                  );
+                }
+              },
+              child: const Text('식단 등록'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
