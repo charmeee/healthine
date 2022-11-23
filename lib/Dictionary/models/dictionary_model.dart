48638633f3a66d13265607872ac6bd2d7a315bd2
charmeee <apple8841193@naver.com>
2022-11-23 19:37:12 +0900

    운동사전 이미지, 영상 추가 및 디자인 적용

--- lib/Dictionary/models/dictionary_model.dart
+++ lib/Dictionary/models/dictionary_model.dart
@@ -4,24 +4,37 @@ class ManualData {
   String enTitle;
   String type;
   // int? difficulty;
+  String? equipmentTitle;
+  String? equipmentId;
+  String? equipmentEnTitle;
   List? description;
-  // List? precautions;
-
-  ManualData({
-    required this.id,
-    required this.title,
-    required this.enTitle,
-    required this.type,
-    // this.difficulty,
-    this.description,
-    //this.precautions
-  });
+  String? precaution;
+  String? imageUrl;
+  String? videoUrl;
+  ManualData(
+      {required this.id,
+      required this.title,
+      required this.enTitle,
+      required this.type,
+      // this.difficulty,
+      this.description,
+      this.precaution,
+      this.equipmentId,
+      this.equipmentTitle,
+      this.equipmentEnTitle,
+      this.imageUrl,
+      this.videoUrl});
   ManualData.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         title = json['title'],
         enTitle = json['enTitle'],
         type = json['type'],
         // difficulty = json['difficulty'],
-        description = (json['description'] ?? "").split('\n');
-  //precautions = json['precautions'];
+        description = (json['description'] ?? "").split('\n'),
+        precaution = json['precaution'],
+        equipmentId = json['equipmentId'],
+        equipmentTitle = json['equipmentTitle'],
+        equipmentEnTitle = json['equipmentEnTitle'],
+        imageUrl = json['imageUrl'],
+        videoUrl = json['videoUrl'];
 }
--- lib/Dictionary/screens/dictionary_detail_screeen.dart
+++ lib/Dictionary/screens/dictionary_detail_screeen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:healthin/Common/Const/const.dart';
+import 'package:healthin/Common/styles/boxStyle.dart';
 import 'package:healthin/Common/styles/textStyle.dart';
 import 'package:healthin/Dictionary/models/dictionary_model.dart';
-import 'package:healthin/Routine/models/routine_models.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DictionaryDetail extends StatelessWidget {
-  ManualData founddata;
+  final ManualData founddata;
 
   DictionaryDetail({Key? key, required this.founddata}) : super(key: key);
 
@@ -14,29 +16,146 @@ class DictionaryDetail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        backgroundColor: fColor,
+        backgroundColor: backgroundColor,
         title: Text(founddata.title.toString()),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Divider(
+            height: 1,
+            color: darkGrayColor,
+          ),
+        ),
       ),
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image.asset("assets/exercise_img/${founddata.id}.png"),
+            if (founddata.imageUrl != null)
+              AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Image.network(imgBaseUrl + founddata.imageUrl!)),
+            Container(
+              decoration: filledContainer,
+              height: 60,
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              padding: EdgeInsets.symmetric(horizontal: 28),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "기구   ",
+                        style: bodyBold_14.copyWith(color: primaryColor),
+                      ),
+                      Text(
+                        founddata.equipmentTitle ?? "---",
+                        style: bodyBold_14,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "타입   ",
+                        style: bodyBold_14.copyWith(color: primaryColor),
+                      ),
+                      Text(
+                        founddata.type.toString(),
+                        style: bodyBold_14,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: founddata.description!
-                      .map((item) => Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Text(
-                              '$item',
-                              style: bodyRegular_16,
-                            ),
-                          ))
-                      .toList()),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: Text(
+                        "운동 방법",
+                        style: bodyBold_16,
+                      ),
+                    ),
+                    for (int i = 0; i < founddata.description!.length; i++) ...[
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          '${(i + 1).toString().padLeft(2, '0')}. ${founddata.description![i]}',
+                          style: bodyRegular_16,
+                        ),
+                      )
+                    ],
+                    SizedBox(
+                      height: 24,
+                    ),
+                    if (founddata.precaution != null) ...[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Text(
+                          "주의점",
+                          style: bodyBold_16,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          '${founddata.precaution}',
+                          style: bodyRegular_16,
+                        ),
+                      )
+                    ]
+                  ]
+                  // founddata.description!
+                  //     .map((item) => Padding(
+                  //           padding: const EdgeInsets.all(4.0),
+                  //           child: Text(
+                  //             '$item',
+                  //             style: bodyRegular_16,
+                  //           ),
+                  //         ))
+                  //     .toList()),
+                  ),
             ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 32),
+              child: TextButton.icon(
+                  onPressed: () {
+                    if (founddata.videoUrl != null) {
+                      launchUrl(Uri.parse(founddata.videoUrl!));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("해당 운동에 대한 영상이 없습니다.")));
+                    }
+                  },
+                  label: Text(
+                    "운동영상",
+                    style: bodyRegular_14.copyWith(color: lightGrayColor),
+                  ),
+                  icon: SvgPicture.asset(
+                    "assets/icons/video.svg",
+                    width: 24,
+                    height: 24,
+                  ),
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.white),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  )),
+            )
           ],
         ),
       ),
