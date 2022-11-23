530ed1abbc7f50fcacccb55a670cfb7629bc1c61
charmeee <apple8841193@naver.com>
2022-11-23 19:46:03 +0900

    루틴상세페이지에서 사전볼수있는 info button과 삭제버튼 기능 추가

--- lib/Routine/widgets/manual_routine_tile.dart
+++ lib/Routine/widgets/manual_routine_tile.dart
@@ -2,6 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:healthin/Common/styles/textStyle.dart';
+import 'package:healthin/Dictionary/models/dictionary_model.dart';
+import 'package:healthin/Dictionary/screens/dictionary_detail_screeen.dart';
+import 'package:healthin/Dictionary/services/dictionary_api.dart';
 import 'package:healthin/Routine/models/routine_models.dart';
 
 import '../../Common/Const/const.dart';
@@ -39,113 +42,141 @@ class ManualTile extends StatefulWidget {
 }
 
 class _ManualTileState extends State<ManualTile> {
+  final routineFormKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ReorderableListView.builder(
-      shrinkWrap: true,
-      padding: EdgeInsets.all(8),
-      itemCount: widget.myRoutine.routineManuals!.length + 1,
-      itemBuilder: (context, index) {
-        if (index == widget.myRoutine.routineManuals!.length) {
-          return Container(
-            key: Key('$index'),
-            height: 50,
-            width: MediaQuery.of(context).size.width,
-            child: Center(
-              child: CircleAvatar(
-                radius: 30,
-                backgroundColor: primaryColor,
-                child: IconButton(
-                    onPressed: () async {
-                      if (widget.recordEmpty) {
-                        List<RoutineManual> fromDictionary =
-                            await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Dictionary(addmode: true)));
-                        widget.setRoutineManauls([
-                          ...widget.myRoutine.routineManuals!,
-                          ...fromDictionary
-                        ]);
-                      } else {
-                        await widget.showDoingRoutineAlert();
-                      }
-                    },
-                    icon: Icon(Icons.add)),
-              ),
-            ),
-          );
-        }
-        RoutineManual routineManual = widget.myRoutine.routineManuals![index];
-        return Container(
-          //key: ValueKey(_routineList["type"]), 는 이름에따라서 키가 발급됨 만약 이름이 같으면 같은키
-          key: Key('$index'),
-          color: Colors.black,
-          padding: EdgeInsets.only(
-              bottom: (index == widget.myRoutine.routineManuals!.length - 1)
-                  ? 0
-                  : 8),
-          child: Container(
-            color: backgroundColor,
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 20),
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              SizedBox(
-                height: 52,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.info_outline,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 12,
-                        ),
-                        Text(
-                          routineManual.manualTitle,
-                          style: bodyBold_16,
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.delete,
-                          color: mediumGrayColor,
-                        )),
-                  ],
+    return Form(
+      key: routineFormKey,
+      child: ReorderableListView.builder(
+        shrinkWrap: true,
+        padding: EdgeInsets.all(8),
+        itemCount: widget.myRoutine.routineManuals!.length + 1,
+        itemBuilder: (context, index) {
+          if (index == widget.myRoutine.routineManuals!.length) {
+            return SizedBox(
+              key: Key('$index'),
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: primaryColor,
+                  child: IconButton(
+                      onPressed: () async {
+                        if (widget.isDoingRoutine && !widget.recordEmpty) {
+                          await widget.showDoingRoutineAlert();
+                        } else {
+                          List<RoutineManual> fromDictionary =
+                              await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          Dictionary(addmode: true)));
+                          widget.setRoutineManauls([
+                            ...widget.myRoutine.routineManuals!,
+                            ...fromDictionary
+                          ]);
+                        }
+                      },
+                      icon: Icon(Icons.add)),
                 ),
               ),
-              Divider(
-                color: darkGrayColor,
-                height: 10,
-              ),
-              routineManual.isCardio
-                  ? _buildCardioRow(routineManual, index)
-                  : _buildWeightRow(routineManual, index),
-            ]),
-          ),
-        );
-      },
-      onReorder: (int oldIndex, int newIndex) {
-        if (widget.isDoingRoutine && !widget.recordEmpty) {
-          widget.showDoingRoutineAlert();
-        } else {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
+            );
           }
-          log("oldIndex : $oldIndex , newIndex : $newIndex");
-          //깊은 복사가 안되었어서 생긴문제 ㅎ;
-          final _routineList = [...widget.myRoutine.routineManuals!];
-          var item = _routineList.removeAt(oldIndex);
-          _routineList.insert(newIndex, item);
-          widget.setRoutineManauls(_routineList);
-        }
-      },
+          RoutineManual routineManual = widget.myRoutine.routineManuals![index];
+          return Container(
+            //key: ValueKey(_routineList["type"]), 는 이름에따라서 키가 발급됨 만약 이름이 같으면 같은키
+            key: Key('$index'),
+            color: Colors.black,
+            padding: EdgeInsets.only(
+                bottom: (index == widget.myRoutine.routineManuals!.length - 1)
+                    ? 0
+                    : 8),
+            child: Container(
+              color: backgroundColor,
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 20),
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                SizedBox(
+                  height: 52,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () async {
+                              ManualData manualData =
+                                  await getDictionaryByManualId(widget.myRoutine
+                                      .routineManuals![index].manualId);
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => DictionaryDetail(
+                                        founddata: manualData,
+                                      )));
+                            },
+                            icon: Icon(
+                              Icons.info_outline,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            routineManual.manualTitle,
+                            style: bodyBold_16,
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            List<RoutineManual> tmp = [
+                              ...widget.myRoutine.routineManuals!
+                            ];
+                            tmp.removeAt(index);
+                            widget.setRoutineManauls(tmp);
+                          },
+                          icon: Icon(
+                            Icons.delete,
+                            color: mediumGrayColor,
+                          )),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: darkGrayColor,
+                  height: 10,
+                ),
+                routineManual.isCardio
+                    ? _buildCardioRow(routineManual, index)
+                    : _buildWeightRow(routineManual, index),
+              ]),
+            ),
+          );
+        },
+        onReorder: (int oldIndex, int newIndex) {
+          if (widget.isDoingRoutine && !widget.recordEmpty) {
+            widget.showDoingRoutineAlert();
+          } else {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            log("oldIndex : $oldIndex , newIndex : $newIndex");
+            //깊은 복사가 안되었어서 생긴문제 ㅎ;
+            final _routineList = [...widget.myRoutine.routineManuals!];
+            var item = _routineList.removeAt(oldIndex);
+            _routineList.insert(newIndex, item);
+            widget.setRoutineManauls(_routineList);
+          }
+        },
+      ),
     );
   }
 
@@ -173,10 +204,9 @@ class _ManualTileState extends State<ManualTile> {
         SizedBox(
           width: 90,
           height: 50,
-          child: TextField(
+          child: TextFormField(
             style: bodyRegular_16,
-            controller: TextEditingController(
-                text: routineManual.playMinute.toString()),
+            initialValue: routineManual.playMinute.toString(),
             textAlign: TextAlign.center,
             keyboardType: TextInputType.number,
             onChanged: (value) {
@@ -196,10 +226,9 @@ class _ManualTileState extends State<ManualTile> {
         SizedBox(
           width: 90,
           height: 50,
-          child: TextField(
+          child: TextFormField(
             style: bodyRegular_16,
-            controller: TextEditingController(
-                text: routineManual.targetNumber.toString()),
+            initialValue: routineManual.targetNumber.toString(),
             textAlign: TextAlign.center,
             keyboardType: TextInputType.number,
             onChanged: (value) {
@@ -251,10 +280,9 @@ class _ManualTileState extends State<ManualTile> {
           SizedBox(
             width: 90,
             height: 50,
-            child: TextField(
+            child: TextFormField(
               style: bodyRegular_16,
-              controller: TextEditingController(
-                  text: routineManual.setNumber.toString()),
+              initialValue: routineManual.setNumber.toString(),
               textAlign: TextAlign.center,
               keyboardType: TextInputType.number,
               onChanged: (value) {
@@ -274,10 +302,9 @@ class _ManualTileState extends State<ManualTile> {
           SizedBox(
             width: 90,
             height: 50,
-            child: TextField(
+            child: TextFormField(
               style: bodyRegular_16,
-              controller:
-                  TextEditingController(text: routineManual.weight.toString()),
+              initialValue: routineManual.weight.toString(),
               textAlign: TextAlign.center,
               keyboardType: TextInputType.number,
               onChanged: (value) {
@@ -298,10 +325,9 @@ class _ManualTileState extends State<ManualTile> {
           SizedBox(
             width: 90,
             height: 50,
-            child: TextField(
+            child: TextFormField(
               style: bodyRegular_16,
-              controller: TextEditingController(
-                  text: routineManual.targetNumber.toString()),
+              initialValue: routineManual.targetNumber.toString(),
               textAlign: TextAlign.center,
               keyboardType: TextInputType.number,
               onChanged: (value) {
