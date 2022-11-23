6584be0cda3182a5a3dd22b2e24b6ecd1367a313
charmeee <apple8841193@naver.com>
2022-11-23 19:38:01 +0900

    운동 사전 manualid로 정보 가져오는 api 추가

--- lib/Dictionary/services/dictionary_api.dart
+++ lib/Dictionary/services/dictionary_api.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter/services.dart';
 import 'package:healthin/Common/dio/dio_main.dart';
 import 'package:healthin/Dictionary/models/dictionary_model.dart';
 
-Future<List<ManualData>> getDicionaryList() async {
+Future<List<ManualData>> getDictionaryList() async {
   //Future<List<DictionaryData>>
   try {
     final response = await dio.get(
@@ -34,7 +32,7 @@ Future<List<ManualData>> getDicionaryList() async {
   }
 }
 
-Future<List<ManualData>> getDicionaryListByEquipmentId(
+Future<List<ManualData>> getDictionaryListByEquipmentId(
     String equipmentId) async {
   //Future<List<DictionaryData>>
   try {
@@ -55,6 +53,27 @@ Future<List<ManualData>> getDicionaryListByEquipmentId(
         throw Exception(e);
       }
       //return _communityList;
+    } else {
+      log("사전 메뉴얼 정보가져오기 오류");
+      throw Exception(response.statusCode);
+    }
+  } catch (e) {
+    throw Exception(e);
+  }
+}
+
+///manuals/{manualId}
+Future<ManualData> getDictionaryByManualId(String manualId) async {
+  //Future<List<DictionaryData>>
+  try {
+    final response = await dio.get(
+      "/manuals/$manualId",
+    );
+    if (response.statusCode == 200) {
+      log("메뉴얼ID로  메뉴얼 정보가져오기 완료");
+      log(response.data.toString());
+      ManualData dictionaryData = ManualData.fromJson(response.data);
+      return dictionaryData;
     } else {
       log("사전 메뉴얼 정보가져오기 오류");
       throw Exception(response.statusCode);
